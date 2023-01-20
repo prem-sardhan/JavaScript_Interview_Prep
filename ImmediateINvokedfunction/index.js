7faa2eb786f5317a7070345776f1c46f5f5a0527
Nandlal <shahnandlal49@gmail.com>
2023-01-20 12:01:00 +0530

    added immediate invoked functions

---
+++ ImmediateINvokedfunction/index.js
@@ -0,0 +1,8 @@
+function normal() {
+   console.log("I am a normal function") 
+}
+normal()
+(()=>{
+console.log("I am a Immediate invoked function")
+})()
+// THis function doesn need any invokation they will invoked as soon as they are  initialized
