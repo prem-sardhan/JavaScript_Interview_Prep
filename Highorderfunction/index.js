16c1165446ff2fc309ae2dd1ea13b7de15b1bbdc
Nandlal <shahnandlal49@gmail.com>
2023-01-20 11:57:15 +0530

    added High order functions

--- Highorderfunction/index.js
+++ Highorderfunction/index.js
@@ -8,7 +8,7 @@
 
 // Foreach function 
 
-let arr=[1,2,3,4,5]
+let arr=[1,2,3,4,5,45,23]
 arr.forEach((el)=>{
 console.log(el)
 })
@@ -24,3 +24,24 @@ let newArr=arr.map((el)=>{
 // It return a new array by transforming that array
 console.log(newArr)
 console.log(arr)
+
+
+// Filter function 
+function canVote(age){
+    return age>=18;
+}
+
+
+let filtered=arr.filter((canVote));
+console.log(filtered)
+
+// It also returns the array but a filter array 
+
+// Reduce Function 
+
+function totalSUm(total=0,num){
+    return total+num;
+}
+let sumhai=arr.reduce(totalSUm)
+console.log(sumhai)
+// It is used when we have to find a single variable it returns a single variable 
