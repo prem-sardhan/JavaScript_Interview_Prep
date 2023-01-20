43445dfbf46d8be3cd3f8059ca12e2bb1db1642e
Nandlal <shahnandlal49@gmail.com>
2023-01-20 18:47:28 +0530

    working on todo app

--- counter/src/Pages/Stopwatch/Stopwatch.js
+++ counter/src/Pages/Stopwatch/Stopwatch.js
@@ -1,5 +1,4 @@
 import React,{useState,useEffect} from 'react'
-import "./Style.css"
 
 function Stopwatch() {
 
--- counter/src/Pages/Todo/Todo.js
+++ counter/src/Pages/Todo/Todo.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function Todo() {
+  const [task, settask] = useState([])
+  const [data, setdata] = useState([])
+  const handlechange=(e)=>{
+console.log(e.target.value)
+settask(e.target.value)
+
+   }
+  console.log(task)
+  const handleaddtask=()=>{
+   setdata(task)
+  }
+  console.log(data)
   return (
-    <div>Todo</div>
+    <div>
+<input type="text" placeholder='Enter your text here' name='todo'  onChange={handlechange}/>
+<span><button onClick={handleaddtask}>Add</button></span>
+<div>
+  {
+  data?.map((el)=>{
+      <p>{el.todo}</p>
+  })
+}
+</div>
+
+    </div>
   )
 }
 
