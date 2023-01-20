4334b0592bbea70541caac6c7288b5b6700220b5
Nandlal <shahnandlal49@gmail.com>
2023-01-20 11:43:57 +0530

    added promise simple method

--- Promise/index.js
+++ Promise/index.js
@@ -16,3 +16,23 @@ sleep(3000).then((res)=>{
 }).catch((err)=>{
     console.log(err)
 })
+
+// Another simple Example of Promise 
+let num=5
+let myPromise = new Promise(function(resolve,reject){
+setTimeout(function() {
+    if(num==5){
+        resolve()
+    }
+    else{
+reject();
+    }
+}, 3000);
+})
+
+myPromise.then(function(){
+console.log("Promise resolved")
+})
+myPromise.catch(function(){
+    console.log("Promise Rejected")
+})
