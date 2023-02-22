aaa205487d955570cf30f92964d6aa55b2f0df11
Nandlal <shahnandlal49@gmail.com>
2023-02-22 14:54:57 +0530

    added call apply and bind method

---
+++ Call,Apply,Bind/index.js
@@ -0,0 +1,55 @@
+let student={
+    name:"AMil",
+    gender:"Male",
+    age:20,
+
+}
+let teacher={
+    name:"Manvandera",
+    gender:"Male",
+    age:29,
+}
+
+function chooseSubject(sub1,sub2){
+    console.log(sub1,sub2);
+}
+
+function getEmail(){
+    return`${this.name} is a ${this.gender} with ${this.age}`
+}
+
+
+console.log(getEmail.call(student))
+console.log(getEmail.call(teacher))
+console.log(chooseSubject.apply(teacher,["science","Maths"]))
+
+
+// It will create a function but that function won't be called
+console.log(getEmail.bind(teacher))
+// It will give a function in result 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// I am using that binded function here in simple we are calling it here 
+let callfunction =getEmail.bind(teacher)
+console.log(callfunction())
+
+// it will give the results here 
