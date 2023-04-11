6c6377a0ff3bca7aa92781b5214b5b60d59d97e7
Nandlal <shahnandlal49@gmail.com>
2023-04-11 14:18:20 +0530

    adding

--- Classes/class.js
+++ Classes/class.js
@@ -15,4 +15,16 @@ class UserProfile{
 let obj=new UserProfile("Amir",34)
 obj.getName()
 
+class Suzuki{
+  constructor(wheel,type){
+  this.wheel=wheel;
+  this.type=type;
+  }
+getCarname(){
+  console.log(`Our car has ${this.wheel} wheels and it is a ${this.type}`)
+}
+}
 
+let final= new Suzuki(4,"sedan")
+console.log(final)
+final.getCarname()
